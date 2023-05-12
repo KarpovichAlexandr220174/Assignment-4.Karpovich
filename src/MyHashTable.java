fa1fa5208d72ef3e297eb60f55bbad8c32a7f96a
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-05-12 19:24:50 +0600

    Comit

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -51,6 +51,14 @@ public class MyHashTable<K, V> {
     }
 
     public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
